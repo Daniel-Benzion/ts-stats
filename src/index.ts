1588f955e1384911177d63816e67ebe88f4fa399
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-11-24 17:58:34 -0600

    MatchResult enum

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,16 @@ const matches = fs.readFileSync('football.csv', {
     return row.split(',');
 });
 
+enum MatchResult {
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D'
+};
+
 let manUnitedWins = 0;
 
 for (let match of matches) {
-    if ((match[1] === 'Man United' && match[5] === 'H') || (match[2] === 'Man United' && match[5] === 'A')) manUnitedWins++; 
+    if ((match[1] === 'Man United' && match[5] === MatchResult.HomeWin) || (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)) manUnitedWins++; 
 }
 
 console.log(`Manchester United won ${manUnitedWins} matches.`);
