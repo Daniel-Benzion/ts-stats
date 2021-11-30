d7d341c698a91f5868b1ac45b783bde4a41c0a59
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-11-30 16:40:48 -0600

    Console report

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
+import { WinsAnalysis } from "./analyzers/WInsAnalysis";
 import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { Summary } from "./Summary";
 
 const csvFileReader = new CsvFileReader('football.csv');
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
+
+const summary = new Summary(
+    new WinsAnalysis('Man United'),
+    new ConsoleReport()
+);
+
+summary.buildAndPrintReport(matchReader.matches);
