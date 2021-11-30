088fba53dbac843055e301127068295d9facc989
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-11-30 16:55:32 -0600

    Static method

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-import { WinsAnalysis } from "./analyzers/WInsAnalysis";
-import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
-import { HtmlReport } from "./reportTargets/HtmlReport";
 import { Summary } from "./Summary";
 
-const csvFileReader = new CsvFileReader('football.csv');
-
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
 const summary = Summary.winsAnalysisWithHtmlReport('Man United');
